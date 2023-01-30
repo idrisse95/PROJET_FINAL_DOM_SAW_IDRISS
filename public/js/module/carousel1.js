f02f04e93f29d495b9701391532919144b67cdf4
55973 <55973@etu.he2b.be>
2023-01-30 19:56:36 +0100

    carousel 1 totalement fonctionnel je brille

--- public/js/module/carousel1.js
+++ public/js/module/carousel1.js
@@ -4,21 +4,22 @@ let butt3 = document.getElementById('buttTrois')
 let butt4 = document.getElementById('buttQuatre')
 
 let carousel1 = document.querySelector('.carou1')
+console.log(carousel1);
 
 butt.addEventListener('click',()=>{
     carousel1.style.transform="translateX(0%)"
 })
 
 butt2.addEventListener('click',()=>{
-    carousel1.style.transform="translateX(100%)"
+    carousel1.style.transform="translateX(-100%)"
 })
 
 
 butt3.addEventListener('click',()=>{
-    carousel1.style.transform="translateX(200%)"
+    carousel1.style.transform="translateX(-200%)"
 })
 
 butt4.addEventListener('click',()=>{
-    carousel1.style.transform="translateX(300%)"
+    carousel1.style.transform="translateX(-300%)"
 })
 
