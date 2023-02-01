69fd94af2e5f939499aaab5472e3cfa62fe30d39
55973 <55973@etu.he2b.be>
2023-02-01 19:33:47 +0100

    dark mode fini

--- public/js/module/modal.js
+++ public/js/module/modal.js
@@ -8,15 +8,66 @@ let nom = document.querySelector('.modal ul li:nth-child(2) input')
 let prenom = document.querySelector('.modal ul li:nth-child(3) input')
 
 
-button.addEventListener('click',()=>{
+button.addEventListener('click', () => {
     modal.style.display = 'block'
 })
 
-enr.addEventListener('click', ()=>{
+enr.addEventListener('click', () => {
     button.textContent = nom.value
     modal.style.display = 'none'
- })
+})
 
-fermer.addEventListener('click', ()=>{
+fermer.addEventListener('click', () => {
     modal.style.display = 'none'
+})
+
+
+
+
+
+
+//DARK MODE
+
+let darkB = document.querySelector('#dark-mode')
+let txtHome = document.querySelectorAll('.blanc')
+let bodyc = document.querySelectorAll('.oui')
+
+let choix = document.querySelectorAll('#choix button')
+let menu = document.querySelectorAll('.carte .items div:nth-child(2)')
+
+let chef = document.querySelectorAll(' .noir')
+
+let i = document.querySelector('#dark-mode i')
+console.log(darkB);
+console.log(bodyc);
+
+darkB.addEventListener('click', () => {
+    bodyc.forEach(element => {
+        element.classList.toggle('dark');
+    });
+    // bodyc.classList.add('dark');
+    console.log('dark');
+    txtHome.forEach(element => {
+        element.classList.toggle('blanche')
+    });
+    choix.forEach(element => {
+        element.classList.toggle('blanche')
+    });
+
+    menu.forEach(element => {
+        element.classList.toggle('blanche')
+    });
+
+    chef.forEach(element => {
+        element.classList.toggle('noire')
+    });
+
+    if (i.classList.contains('fa-moon')) {
+        i.classList.remove('fa-solid')
+        i.setAttribute("class", "fa-regular fa-sun jaune ")
+    }
+    else{
+        i.classList.remove('fa-moon')
+        i.setAttribute("class", "fa-solid fa-moon ")
+    }
 })
